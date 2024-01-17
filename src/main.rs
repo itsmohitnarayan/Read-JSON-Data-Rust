a545c8647587acfa7a0489c82634fd775f073f0f
Kumar Mohit <mohit724196@gmail.com>
2024-01-17 22:24:06 +0530

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct Paragraph{
+    name: String
+}
+#[derive(Serialize, Deserialize)]
+struct Article{
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let json = r#"
+{
+  "article": "how to work with json in Rust",
+  "author": "Mohit Narayan",
+  "paragraph": [
+    {
+      "name": "starting sentences"
+    },
+    {
+      "name": "body of the paragraph"
+    },
+    {
+      "name": "end of the paragraph"
+    }
+  ]
+}
+"#;
+    let parsed: Article = read_json_typed(json);
+    println!("\n\n The name of the first paragraph is: {}", parsed.paragraph[0].name);
+
+}
+
+fn read_json_typed(raw_json: &str) -> Article {
+    let parsed: Article = serde_json::from_str(raw_json).unwrap();
+    return parsed;
 }
